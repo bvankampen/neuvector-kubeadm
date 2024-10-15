586320bba1d544237111a59633ad4c27da8ac250
Bas van Kampen <bas.vankampen@suse.com>
2024-10-15 11:57:08 +0200

    update

---
+++ nfs.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+helm repo add nfs-subdir-external-provisioner https://kubernetes-sigs.github.io/nfs-subdir-external-provisioner/
+helm install nfs-subdir-external-provisioner nfs-subdir-external-provisioner/nfs-subdir-external-provisioner \
+  --set nfs.server=x.x.x.x \
+  --set nfs.path=/exported/path
